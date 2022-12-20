16856d83d3e8c36233207ef5e9bbb49bce9eced3
Amanullah-Nirob <khannirub55@gmail.com>
2022-12-20 15:25:17 +0600

    pwa implementation

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental:{appDir:true},
-  images: {
-    domains: ['res.cloudinary.com'],
-  },
 
-}
-
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	disable: process.env.NODE_ENV === 'development',
+	register: true,
+	scope: '/',
+	sw: 'service-worker.js',
+  })
+  
+module.exports = withPWA({
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental:{appDir:true},
+    images: {
+      domains: ['res.cloudinary.com'],
+    },
+})
