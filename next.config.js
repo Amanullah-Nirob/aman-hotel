d00381c3d11610ed677d98e854ca52c05c587533
Amanullah-Nirob <khannirub55@gmail.com>
2022-12-20 16:45:08 +0600

    try to fix pwa error

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
+const runtimeCaching = require("next-pwa/cache");
 const withPWA = require('next-pwa')({
 	dest: 'public',
 	disable: process.env.NODE_ENV === 'development',
 	register: true,
 	scope: '/',
 	sw: 'service-worker.js',
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/]
   })
   
 module.exports = withPWA({
-    reactStrictMode: true,
-    swcMinify: true,
-    experimental:{appDir:true},
     images: {
       domains: ['res.cloudinary.com'],
     },
